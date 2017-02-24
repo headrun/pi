85a357d8a5bfd2f9ddd8a00cf436643cf72b2989
aravindaddy <aravind@headrun.com>
2017-02-24 14:33:50 +0300

    adding fake and helpful flags

--- PIFramework/juicer/spiders/reviews_data.py
+++ PIFramework/juicer/spiders/reviews_data.py
@@ -12,7 +12,7 @@ class Lixlsfile(object):
         self.excel_file_name = 'reviews_%s.xls'%str(datetime.datetime.now().date())
         self.todays_excel_file = xlwt.Workbook(encoding="utf-8")
         self.todays_excel_sheet1 = self.todays_excel_file.add_sheet("sheet1")
-        header_params = ['source', 'search_keyword', 'title', 'post_text', 'author', 'post_timestamp', 'star_rating', 'count_views', 'count_likes', 'count_comments', 'count_replies', 'count_helpful', 'location', 'flake_flag', 'no_of_reviews', 'review url', 'author_url', 'review_text']
+        header_params = ['source', 'search_keyword', 'title', 'post_text', 'author', 'post_timestamp', 'star_rating', 'count_views', 'count_likes', 'count_comments', 'count_replies', 'count_helpful', 'location', 'flake_flag', 'authors_no_of_reviews', 'review url', 'author_url', 'review_text']
         for i, row in enumerate(header_params):
             self.todays_excel_sheet1.write(0, i, row)
 
@@ -34,7 +34,8 @@ class Lixlsfile(object):
         if conn: conn.close()
 
     def send_xls(self):
-        dbs = ['MOUTHSHUT', 'CONSUMERCOMPLAINTS']
+        #dbs = ['MOUTHSHUT', 'CONSUMERCOMPLAINTS']
+        dbs = ['MOUTHSHUT']
         for db in dbs:
             con2_,cur2_ = self.create_cursor(db, 'root','hdrn59!','localhost')
             cur2_.execute(self.selectqry)
@@ -42,20 +43,29 @@ class Lixlsfile(object):
             for record in records:
                 sk , product_id, name, reviewed_by, reviewed_on, review, review_url, review_rating, aux_info = record
                 aux_infof = {}
-                try: aux_infof = json.loads(aux_info.replace('\\',''))
+                useful = ''
+                try:
+                    aux_infof = re.sub('"useful": "(.*\})",', '', aux_info)
+                    aux_infof = json.loads(aux_infof.replace('\\',''))
+                    try: useful = re.findall('"useful": "(.*\})"', aux_info)[0]
+                    except: pass
                 except: pass
                 keywor = aux_infof.get('browse','').replace('%20',' ').replace('+','')
                 views = aux_infof.get('views', '')
                 likes = aux_infof.get('likes', '')
                 comment = aux_infof.get('no_comments','')
                 if not comment: comment = aux_infof.get('comment','')
+                if not comment: comment = aux_infof.get('no_comments:','')
                 location = aux_infof.get('location','')
                 authorurl = aux_infof.get('author_url','')
                 revits = aux_infof.get('post_title','')
                 noofrev = aux_infof.get('no_of_reviews','')
                 comment = ''.join(re.findall('\d+',comment))
                 noofrev = ''.join(re.findall('\d+',noofrev))
-                values = [db.lower(), keywor, name, review, reviewed_by, reviewed_on, review_rating, views, likes, comment, '', '',  location, '', noofrev, review_url, authorurl, revits]
+                views =  ''.join(re.findall('\d+',views))
+                fake = aux_infof.get('fake','')
+                values = [db.lower(), keywor, name, review, reviewed_by, str(reviewed_on), review_rating, views, likes, comment, '', useful,  location, fake, noofrev, review_url, authorurl, revits]
+                print values
                 for col_count, value in enumerate(values):
                     self.todays_excel_sheet1.write(self.row_count, col_count, value)
                 self.row_count = self.row_count+1
