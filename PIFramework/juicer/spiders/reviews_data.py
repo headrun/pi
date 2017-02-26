33688d54821efbcf925e213b1a956c5e7f2332b9
aravindaddy <aravind@headrun.com>
2017-02-26 14:00:29 +0300

    adding indiaconsumerscript

--- PIFramework/juicer/spiders/reviews_data.py
+++ PIFramework/juicer/spiders/reviews_data.py
@@ -13,10 +13,14 @@ class Lixlsfile(object):
         self.row_count = 1
         self.db_list = self.db_list.split(',')
         self.selectqry = 'select sk , product_id, name, reviewed_by, reviewed_on, review, review_url, review_rating, aux_info from CustomerReviews'
+        self.selectqry1 = 'select sk , product_id, name, reviewed_by, reviewed_on, review, category,  review_url, review_rating, aux_info from CustomerReviews'
         self.excel_file_name = 'reviews_%s.xls'%str(datetime.datetime.now().date())
         self.todays_excel_file = xlwt.Workbook(encoding="utf-8")
         self.todays_excel_sheet1 = self.todays_excel_file.add_sheet("sheet1")
         header_params = ['source', 'search_keyword', 'title', 'post_text', 'author', 'post_timestamp', 'star_rating', 'count_views', 'count_likes', 'count_comments', 'count_replies', 'count_helpful', 'location', 'flake_flag', 'authors_no_of_reviews', 'review url', 'author_url', 'review_text']
+        header_params1 = ['source', 'search_keyword', 'title', 'post_text', 'author', 'post_timestamp','category', 'location', 'review url', 'author_url', 'author_email', 'author_contact_number', 'author_address']
+        if 'INDIA' in ''.join(self.db_list):
+            header_params = header_params1
         for i, row in enumerate(header_params):
             self.todays_excel_sheet1.write(0, i, row)
         self.main()
@@ -42,37 +46,49 @@ class Lixlsfile(object):
         dbs = self.db_list
         for db in dbs:
             con2_,cur2_ = self.create_cursor(db, 'root','hdrn59!','localhost')
-            cur2_.execute(self.selectqry)
+            if 'INDIA' in db:
+                cur2_.execute(self.selectqry1)
+            else:
+                cur2_.execute(self.selectqry)
             records = cur2_.fetchall()
             for record in records:
-                sk , product_id, name, reviewed_by, reviewed_on, review, review_url, review_rating, aux_info = record
-                aux_infof = {}
-                useful = ''
-                try:
-                    aux_infof = re.sub('"useful": "(.*\})",', '', aux_info)
-                    aux_infof = json.loads(aux_infof.replace('\\',''))
-                    try: useful = re.findall('"useful": "(.*\})"', aux_info)[0]
+                if 'INDIA' in db:
+                    sk , product_id, name, reviewed_by, reviewed_on, review, category, review_url, review_rating, aux_info = record
+                else: sk , product_id, name, reviewed_by, reviewed_on, review, review_url, review_rating, aux_info = record
+                if sk:
+                    aux_infof = {}
+                    useful = ''
+                    try:
+                        aux_infof = re.sub('"useful": "(.*\})",', '', aux_info)
+                        aux_infof = json.loads(aux_infof.replace('\\',''))
+                        try: useful = re.findall('"useful": "(.*\})"', aux_info)[0]
+                        except: pass
                     except: pass
-                except: pass
-                keywor = aux_infof.get('browse','').replace('%20',' ').replace('+','')
-                views = aux_infof.get('views', '')
-                likes = aux_infof.get('likes', '')
-                comment = aux_infof.get('no_comments','')
-                if not comment: comment = aux_infof.get('comment','')
-                if not comment: comment = aux_infof.get('no_comments:','')
-                location = aux_infof.get('location','')
-                authorurl = aux_infof.get('author_url','')
-                if not authorurl: authorurl = aux_infof.get('author_profile','')
-                revits = aux_infof.get('post_title','')
-                noofrev = aux_infof.get('no_of_reviews','')
-                comment = ''.join(re.findall('\d+',comment))
-                noofrev = ''.join(re.findall('\d+',noofrev))
-                views =  ''.join(re.findall('\d+',views))
-                fake = aux_infof.get('fake','')
-                values = [db.lower(), keywor, name, review, reviewed_by, str(reviewed_on), review_rating, views, likes, comment, '', useful,  location, fake, noofrev, review_url, authorurl, revits]
-                for col_count, value in enumerate(values):
-                    self.todays_excel_sheet1.write(self.row_count, col_count, value)
-                self.row_count = self.row_count+1
+                    keywor = aux_infof.get('browse','').replace('%20',' ').replace('+','')
+                    views = aux_infof.get('views', '')
+                    likes = aux_infof.get('likes', '')
+                    comment = aux_infof.get('no_comments','')
+                    if not comment: comment = aux_infof.get('comment','')
+                    if not comment: comment = aux_infof.get('no_comments:','')
+                    location = aux_infof.get('location','')
+                    authorurl = aux_infof.get('author_url','')
+                    if not authorurl: authorurl = aux_infof.get('author_profile','')
+                    revits = aux_infof.get('post_title','')
+                    email = aux_infof.get('email','')
+                    address = aux_infof.get('address','')
+                    contact_num = aux_infof.get('contact_no','')
+                    noofrev = aux_infof.get('no_of_reviews','')
+                    comment = ''.join(re.findall('\d+',comment))
+                    noofrev = ''.join(re.findall('\d+',noofrev))
+                    views =  ''.join(re.findall('\d+',views))
+                    fake = aux_infof.get('fake','')
+                    if 'INDIA' in db:
+                        values = [db.lower(), keywor, name, review, reviewed_by, str(reviewed_on), category, location, review_url, authorurl, email, contact_num, address]
+                    else:
+                        values = [db.lower(), keywor, name, review, reviewed_by, str(reviewed_on), review_rating, views, likes, comment, '', useful,  location, fake, noofrev, review_url, authorurl, revits]
+                    for col_count, value in enumerate(values):
+                        self.todays_excel_sheet1.write(self.row_count, col_count, value)
+                    self.row_count = self.row_count+1
             self.close_sql_connection(con2_, cur2_)
         self.todays_excel_file.save(self.excel_file_name)
 
