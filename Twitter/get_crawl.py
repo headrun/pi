5dc1723c053f237595acee037cb4b56114c6f8e1
aravindaddy <aravind@headrun.com>
2017-05-04 07:03:26 +0300

    Adding code to crawl screen wise

---
+++ Twitter/get_crawl.py
@@ -0,0 +1,29 @@
+import os
+import MySQLdb
+
+class Login(object):
+
+    def __init__(self):
+        self.con = MySQLdb.connect(db='TWITTER',
+                              user='root',
+                              passwd='hdrn59!',
+                              charset="utf8",
+                              host='localhost',
+                              use_unicode=True)
+        self.cur = self.con.cursor()
+        self.select_qry = 'select  sk,url,meta_data from twitter_crawl where crawl_status = 0 limit 1'
+        self.update_qry = 'update twitter_crawl set crawl_status=9 where sk= "%s"'
+
+    def main(self):
+                    self.cur.execute(self.select_qry)
+                    rows = self.cur.fetchall()
+                    for row in rows:
+                        args_name,screen_url,email_id=row
+                        cmd = 'python tweet_analyser.py -n %s -e %s'%(args_name,email_id)
+                        self.cur.execute(self.update_qry % args_name)
+                        self.con.commit()
+                        os.system(cmd)
+
+if __name__ == '__main__':
+    Login().main()
+
