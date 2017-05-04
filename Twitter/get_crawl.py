c9aca7396fb9c8b30afae82f1312972dad265b04
aravindaddy <aravind@headrun.com>
2017-05-04 07:20:58 +0300

    Adding code

--- Twitter/get_crawl.py
+++ Twitter/get_crawl.py
@@ -15,14 +15,19 @@ class Login(object):
         self.update_qry = 'update twitter_crawl set crawl_status=9 where sk= "%s"'
 
     def main(self):
-                    self.cur.execute(self.select_qry)
-                    rows = self.cur.fetchall()
-                    for row in rows:
-                        args_name,screen_url,email_id=row
-                        cmd = 'python tweet_analyser.py -n %s -e %s'%(args_name,email_id)
-                        self.cur.execute(self.update_qry % args_name)
-                        self.con.commit()
-                        os.system(cmd)
+        self.cur.execute(self.select_qry)
+        rows = self.cur.fetchall()
+        for row in rows:
+            args_name,screen_url,email_id=row
+            cmd = 'python tweet_analyser.py -n %s -e %s'%(args_name,email_id)
+            self.cur.execute(self.update_qry % args_name)
+            self.con.commit()
+            os.system(cmd)
+
+    def __del__(self):
+        self._db_connection.close()
+        self._db_cur.close()
+
 
 if __name__ == '__main__':
     Login().main()
