9e140ef4b585b7f8aabb61a2fb3f76706f0b4ce7
aravindaddy <aravind@headrun.com>
2017-05-18 03:36:53 -0400

    Adding constants file

---
+++ Facebook/Facebook/spiders/fb_constants.py
@@ -0,0 +1,11 @@
+
+#Users names and passwords
+
+constants_dict = {'anusha1903':['anusha1903','1903@1733'],'sindu':['sindhu4a1@gmail.com','9989365570'],'niki':['nikitha.ccvy.513@gmail.com','dingdongbell@123'],'bala':['balakumaridevara@gmail.com','bala123123'],'pos':['positiveintegersproject@gmail.com','integersproject'],'show':['multishowkiran@gmail.com','kirany@dumbu'],'dummy':['imfacebookdummy01@gmail.com','ram123123'],'fbdummy':['facebookdummyfb01@gmail.com','01123123'],'kolla':['kollaprasanthi1997@gmail.com','515515515'],'pavani':['ccvy1.pavani1886@gmail.com','ccvy1.pavani@1886'],'rama':['srinivasaramanujan427@gmail.com','dotoday1#'],'ch':['cheedellach@gmail.com','cheedellach427'],'anucherry1903':['anucherry1903','1903@1733'], 'kiranmayi':['9290667772','piteamunity'], 'venu':['venu.kallakunta', 'venu@123'], 'smileykutie':['smileykutie@gmail.com','smileykutie$'],'sruthi':['sruthikurra53', 'sruthi^123'], 'na':['nakottamail123@gmail.com', 'nakottamail123#'],'anu':['anushaboyina2@gmail.com','anusha^123']}
+
+#constants_dict = {'niki':['nikitha.ccvy.513@gmail.com','dingdongbell@123'],'bala':['balakumaridevara@gmail.com','bala123123'],'pos':['positiveintegersproject@gmail.com','integersproject'],'show':['multishowkiran@gmail.com','kirany@dumbu'],'dummy':['imfacebookdummy01@gmail.com','ram123123'],'fbdummy':['facebookdummyfb01@gmail.com','01123123'],'kolla':['kollaprasanthi1997@gmail.com','515515515'],'pavani':['ccvy1.pavani1886@gmail.com','ccvy1.pavani@1886'],'rama':['srinivasaramanujan427@gmail.com','dotoday1#'],'ch':['cheedellach@gmail.com','cheedellach427']}
+
+#not active accounts
+
+
+#sindu,rama,smileykutie,fbdummy,sruthi,na,kiranmayi
