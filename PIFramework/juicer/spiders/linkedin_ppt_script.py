0efbbf62bcc21a761d6c2d386957eaa565f242d5
AravindAddy <aravind@headrun.com>
2017-06-21 17:20:45 +0530

    Update linkedin_ppt_script.py

--- PIFramework/juicer/spiders/linkedin_ppt_script.py
+++ PIFramework/juicer/spiders/linkedin_ppt_script.py
@@ -21,7 +21,7 @@ class Login(object):
         self.select_qry = 'select name, sk, summary, headline, location from linkedin_meta where member_id = "%s" '
         self.select_qry1 = 'select exp_title, exp_company_name, start_date, end_date, exp_location from linkedin_experiences \
         where profile_sk ="%s" limit 3'
-        self.select_qry2 = 'select image_path from linkedin_connections where member_id = "%s" limit 1'
+        self.select_qry2 = 'select image_path from linkedin_meta where member_id = "%s" limit 1'
         self.select_qry3 = 'select edu_school_name, edu_degree, edu_start_year, edu_end_year from linkedin_educations \
         where profile_sk="%s" limit 3'
         self.select_qry4 = 'select exp_duration from linkedin_experiences where profile_sk ="%s"'
