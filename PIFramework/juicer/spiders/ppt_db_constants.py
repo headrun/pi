5870f76728de744710161a5ddca6b7bfafea4905
aravindaddy <aravind@headrun.com>
2017-06-27 02:30:47 -0400

    Adding  query too get imagepath

--- PIFramework/juicer/spiders/ppt_db_constants.py
+++ PIFramework/juicer/spiders/ppt_db_constants.py
@@ -12,7 +12,7 @@ db_host='localhost'
 select_qry = 'select name, sk, summary, headline, location from linkedin_meta where member_id = "%s" '
 select_qry1 = 'select exp_title, exp_company_name, start_date, end_date, exp_location from linkedin_experiences \
         where profile_sk ="%s" limit 4'
-select_qry2 = 'select  profile_image from linkedin_meta where member_id = "%s" limit 1'
+select_qry2 = 'select  profile_image,image_path from linkedin_meta where member_id = "%s" limit 1'
 select_qry3 = 'select edu_school_name, edu_degree, edu_start_year, edu_end_year from linkedin_educations \
         where profile_sk="%s" limit 2'
 select_qry4 = 'select exp_duration from linkedin_experiences where profile_sk ="%s"'
