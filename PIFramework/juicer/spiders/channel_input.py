10e35ea901839a4ebd2bd076c202a7e4cbb85bd3
aravindaddy <aravind@headrun.com>
2017-06-27 08:48:25 -0400

    Adding input file

---
+++ PIFramework/juicer/spiders/channel_input.py
@@ -0,0 +1,11 @@
+
+#Inputs to crawl channel info
+
+channel_id = 'UCGBnz-FR3qaowYsyIEh2-zw'
+Access_key = 'AIzaSyBgUp3y8lmbuyCShJvnbuZT9HwX-EH8E8E'
+start_link = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&id=UCGBnz-FR3qaowYsyIEh2-zw&key=AIzaSyBgUp3y8lmbuyCShJvnbuZT9HwX-EH8E8E'
+meta_link = 'https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCGBnz-FR3qaowYsyIEh2-zw&maxResults=50&key=AIzaSyBgUp3y8lmbuyCShJvnbuZT9HwX-EH8E8E'
+ 
+video_link = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId={video Id}&key=AIzaSyBgUp3y8lmbuyCShJvnbuZT9HwX-EH8E8E&maxResults=100'
+
+
