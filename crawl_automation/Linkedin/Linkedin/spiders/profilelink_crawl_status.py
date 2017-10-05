9884952f042f07163bf4200d2a0fca9e11f17a3a
aravindaddy <aravind@headrun.com>
2017-10-05 00:35:31 -0400

    Added code for datetime and total count field

--- crawl_automation/Linkedin/Linkedin/spiders/profilelink_crawl_status.py
+++ crawl_automation/Linkedin/Linkedin/spiders/profilelink_crawl_status.py
@@ -1,35 +1,42 @@
 from stats_gen_functions import *
 from datetime import date, timedelta, time
 import datetime
+import time
 
 class LinkedInScript(object):
     def __init__(self):
 	self.count_list = []
-	self.con = MySQLdb.connect(db='FACEBOOK',user='root',passwd='root',
+	self.con = MySQLdb.connect(db='CRAWL_AUTOMATION',user='root',passwd='root',
                       charset="utf8",host='localhost',use_unicode=True)
 	self.cur = self.con.cursor()
-	self.select_query1 = "SELECT count(*) from linkedin_crawl where crawl_status=%d and created_at='%s'"
         self.main()
 
     def main(self):
-	yesterday = str(date.today() - timedelta(1))+' '+'00:00:00'
+	yesterday = str(date.today() - timedelta(1))
+	self.select_query1 = "SELECT count(*) from linkedin_crawl where crawl_status=%d and date(created_at)='%s'"
 	crawl_status1 = [0,1,9,6]
 	for i in crawl_status1:
-	    select_query1 = self.select_query1%(i,yesterday)
+	    select_query1 = self.select_query1%(i, yesterday)
 	    self.cur.execute(select_query1)
 	    data = self.cur.fetchall()
 	    data = data[0][0]
 	    self.count_list.append(data)
-	today_date = str(datetime.datetime.now())
+	list_ = sum(self.count_list)
+	date2 = str(datetime.datetime.now().date())
+	date1 = datetime.datetime.strptime(date2, '%Y-%m-%d') + datetime.timedelta(days=1)
+	date1 = str(date1).strip('00:00:00')
 	sender_mail = sender_mails
 	receivers_mail_list = receivers_mail_lists
 	sender, receivers  = sender_mail, ','.join(receivers_mail_list)
 	msg = MIMEMultipart('alternative')
-	msg['Subject'] = 'Li crawl automation Stats Generation'+' '+ 'on' +' '+ today_date
+	msg['Subject'] = 'Li crawl automation Stats Generation'+' '+ 'on' +' '+ date1
 	crawl_status = '<html><head><Counts</head>'
 	crawl_status += '<table border="1">'
 	crawl_status += u'<tr> <td><b>Description</b></td>'
 	crawl_status += u'<td><b>Count</b></td>'
+	crawl_status += '</tr>'
+	crawl_status += u'<tr> <td>Total</td>'
+	crawl_status += u'<td>%s</td>'%(list_)
 	crawl_status += '</tr>'
 	crawl_status += u'<tr> <td>No.of urls successfully crawled</td>'
 	crawl_status += u'<td>%s</td>'%(self.count_list[1])
