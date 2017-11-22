b866fe389531a191df9163e1a10d66e7814f3129
aravindaddy <aravind@headrun.com>
2017-11-21 22:44:05 -0500

    Adding lybrate doctors files

---
+++ Practo/juicer/spiders/lybrate_doctorsapi_browse.py
@@ -0,0 +1,172 @@
+from juicer.utils import *
+from juicer.items import DoctorInfo
+from lybrate_doctors_xpaths import *
+import requests
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class LybrateDoctors(JuicerSpider):
+    name = 'lybrate_doctorsapi_browse'
+    handle_http_status_list = ['302', '500', '403']
+    #start_urls = ['https://www.lybrate.com/get/ba/doctors/facet/v2?ampPage=false&cityName=warangal&currentLocation=false&ffR=false&find=&isClinicSearch=false&isHospitalSearch=false&near=&page=2&seoRequest=false&sortBy=BMS&source=FNB&totalPages=&zipSearch=false']
+    def __init__(self, *args, **kwargs):
+        super(LybrateDoctors, self).__init__(*args, **kwargs)
+        self.domain = "https://www.lybrate.com/"
+        self.doctors = '/doctors'
+        self.pattern2 = re.compile('(.*) (.*)')
+        self.week_list = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        self.pagenumber =  kwargs.get('page', '')
+        self.pagenumber = '%s%s'%('?page=',self.pagenumber)
+        self.pageend = kwargs.get('end', '')
+        self.particular_city = kwargs.get('city','')
+        if self.particular_city:
+            #self.start_urls = ['https://www.lybrate.com/%s/doctors'%self.particular_city]
+            self.start_urls = ['https://www.lybrate.com/get/ba/doctors/facet/v2?ampPage=false&cityName=%s&currentLocation=false&ffR=false&find=&isClinicSearch=false&isHospitalSearch=false&near=&page=1&seoRequest=false&sortBy=BMS&source=FNB&totalPages=&zipSearch=false'%self.particular_city]
+        #else:
+            #self.start_urls = ['https://www.lybrate.com/india']
+
+    def parse(self,response):
+        reg = self.particular_city
+        for i in range(1,7):
+            next_page = 'https://www.lybrate.com/get/ba/doctors/facet/v2?ampPage=false&cityName=warangal&currentLocation=false&ffR=false&find=&isClinicSearch=false&    isHospitalSearch=false&near=&page=%s&seoRequest=false&sortBy=BMS&source=FNB&totalPages=&zipSearch=false'%(i)
+            city_url = self.domain+reg+'/doctors?page=%s'%(i)
+            yield Request(next_page, callback=self.parse_next,meta={"city_url":city_url})
+
+    def parse_next(self,response):
+        print response.url
+        city_url = response.meta['city_url']
+        main_data = json.loads(response.body)
+        doc_meta = main_data.get('profileDTOs', [])
+        for doc_info in doc_meta:
+            aux_infos = {}
+            cli_loc = doc_info.get('clinicLocation',{})
+            cli_id = cli_loc.get('id', '')
+            if cli_id:
+                aux_infos.update({"clinic_location_id":cli_id})
+            cli_name = cli_loc.get('name', '').replace(', ','<>')
+            cli_add = cli_loc.get('line1', '')
+            if cli_add: cli_add = cli_add
+            else: cli_add = ''
+            cli_loca = cli_loc.get('localityName', '')
+            if cli_loca: cli_loca = cli_loca
+            else: cli_loca = ''
+            cli_locurl = cli_loc.get('localityUrl', '')
+            cli_locid = cli_loc.get('localityId', '')
+            cli_sta = cli_loc.get('state', '')
+            if cli_sta: cli_sta = cli_sta
+            else : cli_sta= cli_sta
+            cli_pin = cli_loc.get('pincode', '')
+            if cli_pin: cli_pin= cli_pin
+            else: cli_pin = ''
+            clinic_address =cli_add+' '+cli_loca+' '+cli_sta+' '+cli_pin
+            cli_cou = cli_loc.get('country', '')
+            cli_cityid = cli_loc.get('cityId', '')
+            cli_cityna = cli_loc.get('cityName', '')
+            cli_lat = str(cli_loc.get('latitude', ''))
+            if 'None' in cli_lat:
+                cli_lat = ''
+            else: cli_lat = cli_lat
+            cli_lon = str(cli_loc.get('longitude', ''))
+            if 'None' in cli_lon:
+                cli_lon = ''
+            else: cli_lon = cli_lon
+            cli_slug = cli_loc.get('slug', '')
+            cli_preslug = cli_loc.get('preSlugUrl', '')
+            cli_cha = str(cli_loc.get('consultationCharges', ''))
+            if 'None' in cli_cha:
+                cli_cha = ''
+            else: cli_cha = cli_cha
+            cli_nearlocid = cli_loc.get('nearestLocalityId', '')
+            cli_type = cli_loc.get('clinicType', '')
+            if cli_type:
+                aux_infos.update({"clinic_type":normalize(cli_type)})
+            username = doc_info.get('username','')
+            qua = doc_info.get('degrees','')
+            if qua: qua = qua.replace(', ','<>')
+            else: qua= ''
+            spe = doc_info.get('specialityName','')
+            if spe: spe= spe.replace(', ','<>')
+            else: spe=''
+            name = doc_info.get('name','')
+            sub_spe = '<>'.join(doc_info.get('subSpecialities',[]))
+            exp = doc_info.get('experience','')
+            image = doc_info.get('profilePicUrl','')
+            if image: image= image.replace(',','<>')
+            else : image = ''
+            cli_image = doc_info.get('clPhotoDTOs',[])
+            if cli_image:
+                cli_images = '<>'.join([i.get('original','') for i in cli_image])
+            else: cli_images = ''
+            rat = str(doc_info.get('userRatings',''))
+            if 'None' in rat: 
+                rat = ''
+            else: rat = rat
+            popu = str(doc_info.get('userPopularityScore',''))
+            if 'None' in popu:
+                popu = ''
+            else: popu=popu
+            rec_vis = doc_info.get('freqUsedKeywords','')
+            if rec_vis: rec_vis = rec_vis.replace(', ','<>')
+            else: rec_vis = ''
+            feed_co = doc_info.get('feedbackCount','')
+            feed_li = doc_info.get('feedbackLink','')
+            sch_slot,values1 = '',''
+            if feed_li:
+                main_link = feed_li.split('#')[-2]
+            day = doc_info.get('openingHours',{}).keys()
+            values1 = doc_info.get('openingHours',{}).values()
+            hsp_timesch=[]
+            hsp_timesch1 = ''
+            if values1:
+                timee = [''.join(item) for item in values1]
+                for days,timess in zip(day,timee):
+                    day_list=[]
+                    days1 = ''.join(days)
+                    if ',' in days1:
+                        day1 = days1.split(',')
+                        for da in day1:
+                            da1= da.strip(' ')
+                            day_list.append(da1)
+                        for da2 in day_list:
+                            if not ', ' in da2:
+                                hsp_timesch.append(("%s%s%s") % (da2,':-',timess ))
+                    else:
+                        hsp_timesch.append(("%s%s%s") % (days1, ':-',timess ))
+                hsp_timesch1 = '<>'.join(hsp_timesch)
+            reg = self.particular_city
+            #city_url = self.domain+reg+'/doctors?page=2'
+            currency = normalize(u'\u20b9')
+            if username and main_link:
+                print main_link
+                self.get_page('lybrate_doctorsapi_terminal', main_link, username,meta_data={"name":name,"qualification":qua,
+                                "specialization":spe,"years_of_experience":exp,"rating":rat,"vote_count":popu,"feedback_count":feed_co,
+                                "doctor_image":image,"city":reg, "cli_lat":cli_lat,"cli_lon":cli_lon,
+                                "clinic_address":clinic_address,"clic_location":cli_cityna, "cli_images":cli_images})
+            doctor_listing = DoctorInfo()
+            doctor_listing['doctor_id'] = str(username)
+            doctor_listing['doctor_name'] = str(name)
+            doctor_listing['doctor_profile_link'] = str(main_link)
+            doctor_listing['qualification'] = str(qua)
+            doctor_listing['years_of_experience'] = str(exp)
+            doctor_listing['specialization'] = str(spe)
+            doctor_listing['recently_visited_for'] = normalize(rec_vis)
+            doctor_listing['rating'] = str(rat)
+            doctor_listing['vote_count'] = str(popu)
+            doctor_listing['feedback_count'] = str(feed_co)
+            doctor_listing['location'] = normalize(clinic_address)
+            doctor_listing['address'] = str(reg)
+            doctor_listing['consultation_fee'] = str(cli_cha)
+            doctor_listing['schedule_timeslot'] = normalize(hsp_timesch1)
+            doctor_listing['doctor_image'] = str(image)
+            doctor_listing['clinic_names'] = str(cli_name)
+            doctor_listing['clinic_images'] = str(cli_images)
+            doctor_listing['location_latitude'] = str(cli_lat)
+            doctor_listing['location_longitude'] = str(cli_lon)
+            doctor_listing['region'] = str(reg)
+            doctor_listing['fee_currency'] = str(currency)
+            #doctor_listing['booking_type'] = str(doc_booking_type)
+            doctor_listing['reference_url'] = str(city_url)
+            if aux_infos:
+                doctor_listing['aux_info'] = json.dumps(aux_infos)
+            yield doctor_listing
---
+++ Practo/juicer/spiders/lybrate_sheetscript.py
@@ -0,0 +1,205 @@
+from linkedin_functions import *
+
+class Lybratecsv(object):
+    def __init__(self, *args, **kwargs):
+        self.con, self.cur = get_mysql_connection('localhost', 'LYBRATE', '')
+        self.excel_file_name = 'doctor_listing_%s.csv'%str(datetime.datetime.now().date())
+        self.excel_file_name1 = 'doctor_profile_%s.csv'%str(datetime.datetime.now().date())
+        if os.path.isfile(self.excel_file_name):
+            os.system('rm %s'%self.excel_file_name)
+        if os.path.isfile(self.excel_file_name1):
+            os.system('rm %s'%self.excel_file_name1)
+        oupf = open(self.excel_file_name, 'ab+')
+        oupf1 = open(self.excel_file_name1, 'ab+')
+        self.todays_excel_file  = csv.writer(oupf)
+        self.todays_excel_file1 = csv.writer(oupf1)
+        self.file_dirs = os.path.join(os.getcwd(),'OUTPUT')
+        self.QUERY_FILES_DIR = os.path.join(self.file_dirs, 'load_script/processing')
+        self.QUERY_FILES_CRAWLOUT_DIR = os.path.join(self.file_dirs, 'load_script/crawl_out')
+        self.tableschema = 'INSERT INTO practo_final_table ('
+        self.na = 'practo_final_table'
+        self.tables_file = self.get_tables_file()
+        self.query1 = 'select * from %s where date(modified_at) >= "2017-11-20"'
+        self.query2 = 'select * from %s where %s = "%s"'
+        self.columns_query = 'SELECT COLUMN_NAME FROM information_schema.columns where table_schema= "LYBRATE" and table_name = "%s"'
+        self.max_count_query = "select count(*)  from %s group by doctor_id order by count(*) desc limit 1"
+        doct_info_columns = fetchmany(self.cur, self.columns_query%('DoctorInfo'))
+        doc_info_list = list(doct_info_columns)
+        self.doc_info_uplist = list(chain.from_iterable(doc_info_list))[:-3]
+        self.doc_info_uplist.extend(['phone_number', 'extension', 'city', 'Availability Text'])
+        doct_meta_columns = fetchmany(self.cur, self.columns_query%('DoctorMeta'))
+        doct_meta_ls = list(doct_meta_columns)
+        self.doc_meta_uplist = list(chain.from_iterable(doct_meta_ls))[1:-3]
+        hospital_columns = fetchmany(self.cur, self.columns_query%('DoctorHospital'))
+        doct_hospitals = list(hospital_columns)
+        self.doct_hospitals = list(chain.from_iterable(doct_hospitals))[2:-4]
+        feedback_columns = fetchmany(self.cur, self.columns_query%('DoctorFeedback'))
+        feed_col = list(feedback_columns)
+        self.feedback_col = list(chain.from_iterable(feed_col))[3:-4]
+        self.columns_pft = ''
+
+        self.headerlisting = []
+        self.headerprofiles = []
+        self.main()
+
+    def get_current_ts_with_ms(self):
+        dt = datetime.datetime.now().strftime("%Y%m%dT%H%M%S%f")
+        return dt
+    def get_tables_file(self):
+        tables_queries_filename = os.path.join(self.QUERY_FILES_CRAWLOUT_DIR, "%s_%s.queries" % (self.na, self.get_current_ts_with_ms()))
+        self.tables_file = open(tables_queries_filename, 'w')
+        return self.tables_file
+
+    def move_file(self, source, dest):
+        cmd = "mv %s %s" % (source, dest)
+        os.system(cmd)
+
+
+    def close_all_opened_query_files(self):
+        files_list = [self.tables_file]
+
+        for f in files_list:
+            if not isinstance(f, file): continue
+            f.flush()
+            f.close()
+            self.move_file(f.name, self.QUERY_FILES_DIR)
+
+
+
+    def send_csv(self):
+        records = fetchall(self.cur, self.query1%('DoctorInfo'))
+        counterpa = 0
+        for inde, rec in enumerate(records):
+            info_rec = list(rec)[:-2]
+            phone_no, extension, city_name = ['']*3
+            try:
+                jsd = json.loads(info_rec[-1])
+                if jsd:
+                    phone_no = jsd.get('phone_number','')
+                    extension = jsd.get('extension','')
+                    city_name = jsd.get('city', '')
+                    availability_text = jsd.get('doc_avilability_text','')
+            except:
+                pass
+
+            info_rec[-1] = phone_no
+            info_rec.extend([extension])
+            info_rec.extend([city_name])
+            info_rec.extend([availability_text])
+            print info_rec[0], '>>>>'
+            counterpa+=1
+            print counterpa
+            #if '316500' in info_rec[0]  or '555819' in info_rec[0]: continue
+            if inde == 0:
+                self.headerlisting.extend(self.doc_info_uplist)
+                self.headerprofiles.extend(self.doc_meta_uplist)
+                #import pdb;pdb.set_trace()
+                self.headerprofiles.extend(['city'])
+                self.todays_excel_file.writerow(self.headerlisting)
+            info_rec = [normalize(i) for i in info_rec]
+            self.todays_excel_file.writerow(info_rec)
+            values_final = []
+            doctor_id = info_rec[0]
+            meta_ = fetchall(self.cur, self.query2%("DoctorMeta", 'sk', doctor_id))
+            meta_rec = []
+            status = 'Available'
+            if meta_:
+                meta_rec = list(meta_[0])[1:-2]
+                meta_rec[-1] = json.loads(meta_rec[-1]).get('city', '')
+            else:
+                meta_rec = ['' for i in range(23)]
+                meta_rec[0] = doctor_id
+                status = 'Not Available'
+            values_final.extend(meta_rec)
+            doctor_meta_id = meta_rec[0]
+            doct_hospitals = self.values_for(inde, doctor_meta_id)
+            values_final.extend(doct_hospitals)
+            callfun = self.querydesign(doctor_meta_id, inde)
+            if inde == 0:
+                self.headerprofiles.extend(['Status'])
+                self.todays_excel_file1.writerow(self.headerprofiles)
+                table_schema =  "%s%s%s"%("CREATE TABLE `practo_final_table` (`id` int(11) NOT NULL AUTO_INCREMENT, ",''.join(["%s%s%s%s"%('`' , i, '`',' text COLLATE utf8_unicode_ci, ') for i in self.headerprofiles]), "`created_at` datetime NOT NULL,   `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,   `last_seen` datetime NOT NULL,   PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci; ")
+                execute_query(self.cur, table_schema)
+                columns_pfinalt = fetchmany(self.cur, self.columns_query%('practo_final_table'))
+                self.columns_pft = list(chain.from_iterable(list(columns_pfinalt)[1:-3]))
+            orig_vals = values_final
+            countr=0
+            final_qryto = "%s%s%s%s%s%s"%(self.tableschema,', '.join(self.columns_pft), ', created_at, modified_at, last_seen) values (' , ', '.join(['%s' for i in range(len(self.columns_pft))]), ', now(), now(), now()) ON DUPLICATE KEY UPDATE last_seen=now(), ', ', '.join([str(i)+'=%s' for i in self.columns_pft]))
+            for cf in callfun:
+                countr+=1
+                uptovalues_here = orig_vals
+                uptovalues_here.extend(cf)
+                uptovalues_here.extend([status])
+                uptovalues_here =  [normalize(i) for i in uptovalues_here]
+                if len(self.headerprofiles) != len(uptovalues_here):
+                    print len(self.headerprofiles)
+                    print len(uptovalues_here)
+                self.todays_excel_file1.writerow(uptovalues_here)
+                final_table_values = []
+                final_table_values = uptovalues_here+uptovalues_here
+                #execute_query(self.cur, "%s%s"%(final_qryto, tuple(final_table_values)))
+                self.tables_file.write('%s\n%s\n' %(final_qryto, tuple(final_table_values)))
+                self.tables_file.flush()
+                del orig_vals[-13:]
+
+
+            """values_final.extend([callfun])
+            if inde == 0: self.todays_excel_file1.writerow(self.headerprofiles)
+            values_final =  [normalize(i) for i in values_final]
+            self.todays_excel_file1.writerow(values_final)"""
+        self.close_all_opened_query_files()
+
+
+    def querydesign(self, doctor_meta_id, hindex):
+        if hindex == 0: self.headerprofiles.extend(self.feedback_col)
+        final_to_update = []
+        values = fetchmany(self.cur, self.query2%("DoctorFeedback", 'doctor_id', doctor_meta_id))
+        for ind, val in enumerate(values):
+            vals_ = list(val)[3:-4]
+            final_to_update.append(vals_)
+        if not final_to_update:
+            final_to_update.append(['' for i in self.feedback_col])
+        return final_to_update
+        """if hindex == 0:  self.headerprofiles.extend(['Feedback'])
+        values = fetchmany(self.cur, self.query2%("DoctorFeedback", 'doctor_id', doctor_meta_id))
+        final_to_update = []
+        for ind, val in enumerate(values):
+                vals_ = list(val)[2:-4]
+                indes = '%s%s'%('_',str(ind+1))
+                feed_ = self.feedback_col
+                feedb_= map(lambda x:x+indes, feed_)
+                valf = filter(None, map(lambda a,b: (a+':-'+b) if b else '', feedb_,vals_))
+                final_to_update.append(', '.join(valf))
+        return ' <> '.join(final_to_update) """
+
+
+
+    def values_for(self, inde, doctor_meta_id):
+        large_count = fetchmany(self.cur, self.max_count_query%('DoctorHospital'))
+        max_count = ''
+        try: max_count = int(large_count[0][0])
+        except: max_count = ''
+        va = []
+        if max_count:
+            for fi in range(1,max_count+1):
+                for fl in self.doct_hospitals:
+                    va.append("%s%s%s%s"%('hospital_', str(fi), '_', fl.split('hospital_')[-1]))
+        if inde == 0: self.headerprofiles.extend(va)
+        countrec = fetchmany(self.cur, self.query2%("DoctorHospital", 'doctor_id', doctor_meta_id))
+        cntf_ = []
+        if countrec:
+                cntf = map(lambda x:(x[2:-4]), countrec)
+                cntf_ = list(chain.from_iterable(cntf))
+        if len(cntf_) != len(va):
+                lnewln = len(va) - len(cntf_)
+                cntf_.extend(['']*lnewln)
+        return cntf_
+
+    def main(self):
+        self.send_csv()
+
+if __name__ == '__main__':
+        parser = optparse.OptionParser()
+        parser.add_option('-m', '--modified-at', default='', help = 'modified_at')
+        (options, args) = parser.parse_args()
+        Lybratecsv(options)
